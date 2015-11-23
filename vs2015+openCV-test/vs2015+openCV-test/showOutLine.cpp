4362d6cce04ced53def658cf2573655d5486254b
lzd-pc <18547186396@163.com>
2015-11-23 16:21:17 +0800

    添加滤波

--- vs2015+openCV-test/vs2015+openCV-test/showOutLine.cpp
+++ vs2015+openCV-test/vs2015+openCV-test/showOutLine.cpp
@@ -80,7 +80,229 @@ void FillInternalContours(IplImage *pBinary, double dAreaThre)
 		pStorage = NULL;
 	}
 }
-int main()
+
+
+/*======================================================================*/
+ /* OTSU global thresholding routine */
+ /*======================================================================*/
+int otsu2(IplImage *image)
+{
+	int w = image->width;
+	int h = image->height;
+
+	unsigned char*np; // 图像指针  
+	unsigned char pixel;
+	int thresholdValue = 1; // 阈值  
+	int ihist[256]; // 图像直方图，256个点  
+
+	int i, j, k; // various counters  
+	int n, n1, n2, gmin, gmax;
+	double m1, m2, sum, csum, fmax, sb;
+
+	// 对直方图置零...  
+	memset(ihist, 0, sizeof(ihist));
+
+	gmin = 255; gmax = 0;
+	// 生成直方图  
+	for (i = 0; i < h; i++)
+	{
+		np = (unsigned char*)(image->imageData + image->widthStep*i);
+		for (j = 0; j < w; j++)
+		{
+			pixel = np[j];
+			ihist[pixel]++;
+			if (pixel > gmax) gmax = pixel;
+			if (pixel < gmin) gmin = pixel;
+		}
+	}
+
+	// set up everything  
+	sum = csum = 0.0;
+	n = 0;
+
+	for (k = 0; k <= 255; k++)
+	{
+		sum += k * ihist[k]; /* x*f(x) 质量矩*/
+		n += ihist[k]; /* f(x) 质量 */
+	}
+
+	if (!n)
+	{
+		// if n has no value, there is problems...  
+		//fprintf (stderr, "NOT NORMAL thresholdValue = 160\n");  
+		thresholdValue = 160;
+		goto L;
+	}
+
+	// do the otsu global thresholding method  
+	fmax = -1.0;
+	n1 = 0;
+	for (k = 0; k <255; k++)
+	{
+		n1 += ihist[k];
+		if (!n1) { continue; }
+		n2 = n - n1;
+		if (n2 == 0) { break; }
+		csum += k *ihist[k];
+		m1 = csum / n1;
+		m2 = (sum - csum) / n2;
+		sb = n1 * n2 *(m1 - m2) * (m1 - m2);
+		/* bbg: note: can be optimized. */
+		if (sb > fmax)
+		{
+			fmax = sb;
+			thresholdValue = k;
+		}
+	}
+
+L:
+	for (i = 0; i < h; i++)
+	{
+		np = (unsigned char*)(image->imageData + image->widthStep*i);
+		for (j = 0; j < w; j++)
+		{
+			if (np[j] >= thresholdValue)
+				np[j] = 255;
+			else np[j] = 0;
+		}
+	}
+
+	//cout<<"The Threshold of this Image in Otsu is:"<<thresholdValue<<endl;  
+	return(thresholdValue);
+}
+
+void useOtsu2(IplImage *imgOtsu)
+{
+	
+	int thre2;
+	 thre2 = otsu2(imgOtsu);
+	 cout << "The Threshold of this Image in Otsu is:" << thre2 << endl;//输出显示阀值  
+	 cvThreshold(imgOtsu, imgOtsu, thre2, 255, CV_THRESH_BINARY); // 二值化   
+	 cvNamedWindow("imgOtsu", CV_WINDOW_AUTOSIZE);
+	 //cvShowImage("imgOtsu", imgOtsu);//显示图像  
+}
+/*============================================================================
+ = 代码内容：最大熵阈值分割
+ = 修改日期:2009-3-3
+ = 作者:crond123
+ = 博客:http://blog.csdn.net/crond123/
+ = E_Mail:crond123@163.com
+ ===============================================================================*/
+// 计算当前位置的能量熵  
+int HistogramBins = 256;
+float HistogramRange1[2] = { 0,255 };
+float *HistogramRange[1] = { &HistogramRange1[0] };
+typedef
+enum { back, object } entropy_state;
+
+
+
+double caculateCurrentEntropy(CvHistogram * Histogram1, int cur_threshold, entropy_state state)
+{
+	int start, end;
+	if (state == back) {
+		start = 0; end = cur_threshold;
+	}
+	else {
+		start = cur_threshold; end = 256;
+	}
+	int  total = 0;
+	for (int i = start; i<end; i++) {
+		total += (int)cvQueryHistValue_1D(Histogram1, i);
+	}
+	double cur_entropy = 0.0;
+	for (int i = start; i<end; i++) {
+		if ((int)cvQueryHistValue_1D(Histogram1, i) == 0)
+			continue;
+		double percentage = cvQueryHistValue_1D(Histogram1, i) / total;
+		cur_entropy += -percentage*logf(percentage);
+	}
+	return cur_entropy;
+}
+void  MaxEntropy(IplImage *src, IplImage *dst)
+{
+	assert(src != NULL);
+	assert(src->depth == 8 && dst->depth == 8);
+	assert(src->nChannels == 1);
+	CvHistogram * hist = cvCreateHist(1, &HistogramBins, CV_HIST_ARRAY, HistogramRange);
+	cvCalcHist(&src, hist);
+	double maxentropy = -1.0;
+	int max_index = -1;
+	for (int i = 0; i<HistogramBins; i++) {
+		double cur_entropy =
+			caculateCurrentEntropy(hist, i, object) + caculateCurrentEntropy(hist, i, back);
+		if (cur_entropy>maxentropy) {
+			maxentropy = cur_entropy;
+			max_index = i;
+		}
+	}
+	cvThreshold(src, dst, (double)max_index, 255, CV_THRESH_BINARY);
+	//cvReleaseHist(&hist);
+}
+void useMaxEntropy(IplImage *imgMaxEntropy)
+{
+	 MaxEntropy(imgMaxEntropy, imgMaxEntropy);
+	 cvNamedWindow("MaxEntroyThreshold", CV_WINDOW_AUTOSIZE);
+	 cvShowImage("MaxEntroyThreshold", imgMaxEntropy);//显示图像  
+}
+
+
+/*============================================================================
+ = 代码内容：基本全局阈值法
+ ==============================================================================*/
+int BasicGlobalThreshold(int*pg, int start, int end)
+{ // 基本全局阈值法  
+	int i, t, t1, t2, k1, k2;
+	double u, u1, u2;
+	t = 0;
+	u = 0;
+	for (i = start; i<end; i++)
+	{
+		t += pg[i];
+		u += i*pg[i];
+	}
+	k2 = (int)(u / t); // 计算此范围灰度的平均值   
+	do
+	{
+		k1 = k2;
+		t1 = 0;
+		u1 = 0;
+		for (i = start; i <= k1; i++)
+		{ // 计算低灰度组的累加和  
+			t1 += pg[i];
+			u1 += i*pg[i];
+		}
+		t2 = t - t1;
+		u2 = u - u1;
+		if (t1)
+			u1 = u1 / t1; // 计算低灰度组的平均值  
+		else
+			u1 = 0;
+		if (t2)
+			u2 = u2 / t2; // 计算高灰度组的平均值  
+		else
+			u2 = 0;
+		k2 = (int)((u1 + u2) / 2); // 得到新的阈值估计值  
+	} while (k1 != k2); // 数据未稳定，继续  
+						//cout<<"The Threshold of this Image in BasicGlobalThreshold is:"<<k1<<endl;  
+	return(k1); // 返回阈值  
+}
+
+void use(IplImage *imgBasicGlobalThreshold)
+{
+	int pg[256], i, thre;
+	for (i = 0; i<256; i++) pg[i] = 0;
+	for (i = 0; i<imgBasicGlobalThreshold->imageSize; i++) // 直方图统计  
+		pg[(BYTE)imgBasicGlobalThreshold->imageData[i]]++;
+	thre = BasicGlobalThreshold(pg, 0, 256); // 确定阈值  
+	cout << "The Threshold of this Image in BasicGlobalThreshold is:" << thre << endl;//输出显示阀值  
+	cvThreshold(imgBasicGlobalThreshold, imgBasicGlobalThreshold, thre, 255, CV_THRESH_BINARY); // 二值化   
+	cvNamedWindow("BasicGlobalThreshold", CV_WINDOW_AUTOSIZE);
+	cvShowImage("BasicGlobalThreshold", imgBasicGlobalThreshold);//显示图像  
+}
+
+//找到运动物体
+void show() 
 {
 	IplImage* pFrame = NULL;
 	IplImage* pFrImg = NULL;
@@ -100,6 +322,7 @@ int main()
 	cvMoveWindow("foreground", 690, 0);
 
 	pCapture = cvCaptureFromFile("d:\\测试\\VID_20150914_160002.mp4");
+	//	pCapture = cvCreateCameraCapture(0);
 	while (pFrame = cvQueryFrame(pCapture))
 	{
 		nFrmNum++;
@@ -125,11 +348,14 @@ int main()
 			graFilterMid(pFrImg, 1);
 			cvConvert(pFrImg, pFrameMat);
 			//先做高斯滤波，以平滑图像  
-			cvSmooth(pFrameMat, pFrameMat, CV_GAUSSIAN, 3, 0, 0);
+			
+			cvSmooth(pFrImg, pFrImg, CV_GAUSSIAN, 3, 0, 0);
 			
 			//当前帧跟背景图相减  
 			cvAbsDiff(pFrameMat, pBkMat, pFrMat);
 			//二值化前景图  
+			//use(pFrImg);
+			//useMaxEntropy(pFrImg);
 			cvThreshold(pFrMat, pFrImg, 10, 255.0, CV_THRESH_BINARY);
 			////进行形态学滤波，去掉噪音
 			cvErode(pFrImg, pFrImg, 0, 6);
@@ -142,16 +368,17 @@ int main()
 			pFrame->origin = IPL_ORIGIN_BL;
 			pFrImg->origin = IPL_ORIGIN_BL;
 			pBkImg->origin = IPL_ORIGIN_BL;
+			
 			FillInternalContours(pFrImg, 300);
 			cvShowImage("video", pFrame);
-			cvShowImage("background", pBkImg);
+			//cvShowImage("background", pBkImg);
 			cvShowImage("foreground", pFrImg);
 			//如果有按键事件，则跳出循环  
 			//此等待也为cvShowImage函数提供时间完成显示  
 			//等待时间可以根据CPU速度调整  
 			if (cvWaitKey(2) >= 0)
 				break;
-			
+
 
 		}
 
@@ -170,5 +397,11 @@ int main()
 
 	cvReleaseCapture(&pCapture);
 
+}
+
+int main()
+{
+	//ss();
+	show();
 	return 0;
 }
--- vs2015+openCV-test/vs2015+openCV-test/stdafx.h
+++ vs2015+openCV-test/vs2015+openCV-test/stdafx.h
@@ -10,14 +10,23 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#include<highgui.h>
 #include <opencv/cv.h>
 #include <opencv/cvaux.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
+#include<opencv2\highgui\highgui.hpp>
 #include <iostream>
 #include <stdio.h>
 #include <stdarg.h>
 #include <math.h>
+#include <iostream>
+using namespace std;
+
+#define BYTE unsigned char
+
+#define cvQueryHistValue_1D( hist, idx0 ) \
+    ((float)cvGetReal1D( (hist)->bins, (idx0)))
 
 
 
